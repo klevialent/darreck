243a733372d75f05c663497924e880e00578b284
klevialent <klevialent@ya.ru>
2024-12-27 19:31:57 +0300

    stop var margin calculation

--- config/sсheduler.exs
+++ config/sсheduler.exs
@@ -4,6 +4,6 @@ config :darreck, Darreck.Scheduler,
   jobs: [
     {"14 11,16 * * *",      {DarreckSchedule.PortfolioStat, :run, []}},
     {"52 20 * * *",         {DarreckSchedule.PortfolioStat, :run, []}},
-    {"00 11,16 * * *",      {DarreckSchedule.VarMargin, :set_position_cost, []}},
-    {"15 11,16 * * *",      {DarreckSchedule.VarMargin, :set_var_margin, []}},
+    # {"00 11,16 * * *",      {DarreckSchedule.VarMargin, :set_position_cost, []}},
+    # {"15 11,16 * * *",      {DarreckSchedule.VarMargin, :set_var_margin, []}},
   ]
