848076af3b8f049e2c324c4c8b629fe000acc75a
klevialent <klevialent@ya.ru>
2025-01-13 16:02:46 +0300

    debug force ssl

--- config/prod.exs
+++ config/prod.exs
@@ -7,7 +7,7 @@ import Config
 # before starting your production server.
 config :darreck, DarreckWeb.Endpoint,
   cache_static_manifest: "priv/static/cache_manifest.json",
-  force_ssl: [rewrite_on: [:x_forwarded_proto], host: nil]
+  force_ssl: [rewrite_on: [:x_forwarded_proto], host: nil, log: :debug]
 
 # Configures Swoosh API Client
 config :swoosh, api_client: Swoosh.ApiClient.Finch, finch_name: Darreck.Finch
