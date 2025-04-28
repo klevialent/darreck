e8b9ba6b314f4c6ce1b46d787b4e04e75c3e16bf
klevialent <klevialent@ya.ru>
2025-04-28 19:22:16 +0300

    dont save stat

--- lib/darreck_schedule/portfolio_stat.ex
+++ lib/darreck_schedule/portfolio_stat.ex
@@ -6,14 +6,14 @@ defmodule DarreckSchedule.PortfolioStat do
   def run() do
     stat = DarreckTiapi.Portfolio.stat()
 
-    Schema.changeset(%Schema{}, %{
-      worth: stat.worth,
-      cash: stat.cash_without_guarantee,
-      long: stat.long_all,
-      short: stat.short_all,
-      pin: stat.long_pinned_futures,
-    })
-    |> Darreck.Repo.insert()
+    # Schema.changeset(%Schema{}, %{
+    #   worth: stat.worth,
+    #   cash: stat.cash_without_guarantee,
+    #   long: stat.long_all,
+    #   short: stat.short_all,
+    #   pin: stat.long_pinned_futures,
+    # })
+    # |> Darreck.Repo.insert()
 
 
     Telegex.send_message(DarreckTgBot.chat_id, Response.do_response(stat), parse_mode: "html")
